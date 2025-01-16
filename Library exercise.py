220350857f7beb49ebeff33467b619fffeb38007
ctrlsina <nandamellenia@gmail.com>
2025-01-16 19:22:20 +0700

    Update Library exercise.py
    - added non-string title check
    - added enumeration on display shelf/collection

--- Library exercise.py
+++ Library exercise.py
@@ -6,11 +6,14 @@ class Library:
     def add_book(self, title):
         '''Adding book by title'''
         try:
-            if title == '': #Ensuring title is non-empty string
-                print('Please input valid book title')
+            if isinstance(title,str):
+                if title == '': #Ensuring title is non-empty string
+                    print('Please input valid book title')
+                else:
+                    self.books.append(title)
+                    print(f'Your book is successfully added to the collection!\nBook Added:{title}\nHere are the list of your collection:{self.books}')
             else:
-                self.books.append(title)
-                print(f'Your book is successfully added to the collection!\nBook Added:{title}\nHere are the list of your collection:{self.books}')
+                print('Please input valid book title')
         except TypeError:
             print('Please input valid book title')
         except ValueError:
@@ -19,21 +22,27 @@ class Library:
     def remove_book(self,title):
         '''Remove book by title'''
         try:
-            if title in self.books: #Checking if book is in the collection
-                self.books.remove(title)
+            if isinstance(title,str):
+                if title in self.books: #Checking if book is in the collection
+                    self.books.remove(title)
+                else:
+                    print(f'Book is not in the collection. Current collection:{self.books}')
             else:
-                print(f'Book is not in the collection. Current collection:{self.books}')
+                print('Please input valid book title.')
         except TypeError:
             print('Please input valid book title.')
         except ValueError:
             print('Please input valid book title.')
     
     def display_books(self):
-        print(f'Here are books in your collection: {self.books}')
+
+        print('Here are books in your collection:')
+        for index, book in enumerate(self.books, start=1):
+            print(f'{index}. {book}')
 
 library1 = Library()
 library1.add_book('1984')
 library1.add_book('Milk and honey')
 library1.remove_book('1984')
-library1.add_book('')
+library1.add_book('To kill a mockingbird')
 library1.display_books()
